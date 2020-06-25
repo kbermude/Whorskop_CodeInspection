d1d3b4d3438f09f0623eeafed7565d61a14db363
kbermude <kmbmha@hotmail.com>
2020-06-25 09:32:28 -0500

    pmd report fixed

--- CodeInspectionSE2/src/main/java/labcodeinspection/Email.java
+++ CodeInspectionSE2/src/main/java/labcodeinspection/Email.java
@@ -1,26 +1,26 @@
 package labcodeinspection;
 
-public class Email {
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
-	private String department;
-	private int defaultpasswordLength = 8;
-	private String email;
+public class Email { // NOPMD 
+	private String m_firstName; // NOPMD 
+	private String m_lastName; // NOPMD 
+	private String password = null; // NOPMD 
+	private String department; // NOPMD 
+	private int defaultpasswordLength = 8; // NOPMD 
+	private String email; // NOPMD 
 
-	public Email(String firstName, String lastName) {
+	public Email(String firstName, String lastName) { // NOPMD 
 		this.m_firstName = firstName;
 		this.m_lastName = lastName;
 	}
 
-	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+	public void showInfo() { // NOPMD 
+		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName); // NOPMD 
+		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password); // NOPMD 
 	}
 
-	public void setDeparment(int depChoice) {
-		switch (depChoice) {
+	public void setDeparment(int depChoice) { // NOPMD 
+		switch (depChoice) { // NOPMD 
 		case 1:
 			this.department = "sales";
 			break;
@@ -33,19 +33,19 @@ public class Email {
 		}
 	}
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
-		char[] password = new char[length];
+	private String randomPassword(int length) { // NOPMD
+		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*"; // NOPMD 
+		char[] password = new char[length]; // NOPMD 
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
-			password[i] = set.charAt(rand);
+			int rand = (int) (Math.random() * set.length()); // NOPMD 
+			password[i] = set.charAt(rand); // NOPMD 
 		}
 		return new String(password);
 	}
 
-	public void generateEmail() {
+	public void generateEmail() { // NOPMD by Karen on 6/25/20 9:19 AM
 		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
+		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department // NOPMD 
 				+ ".espol.edu.ec";
 	}
 }
--- CodeInspectionSE2/src/main/java/labcodeinspection/EmailApp.java
+++ CodeInspectionSE2/src/main/java/labcodeinspection/EmailApp.java
@@ -3,23 +3,23 @@ package labcodeinspection;
 import java.util.Scanner;
 
 
-public class EmailApp {
+public class EmailApp { // NOPMD
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) { // NOPMD 
+		Scanner sc = new Scanner(System.in); // NOPMD 
 
-		System.out.print("Enter your first name: ");
-		String firstName = sc.nextLine();
+		System.out.print("Enter your first name: "); // NOPMD 
+		String firstName = sc.nextLine(); // NOPMD 
 
-		System.out.print("Enter your last name: ");
-		String lastName = sc.nextLine();
+		System.out.print("Enter your last name: "); // NOPMD 
+		String lastName = sc.nextLine(); // NOPMD 
 
-		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
+		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: "); // NOPMD 
 
-		int depChoice = sc.nextInt();
+		int depChoice = sc.nextInt(); // NOPMD 
 		sc.close();
 
-		Email email = new Email(firstName, lastName);
+		Email email = new Email(firstName, lastName); // NOPMD
 		email.setDeparment(depChoice);
 		email.generateEmail();
 		email.showInfo();
